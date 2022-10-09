aee406c659234a5248f016b2216421a521d0357c
Alexander Kozlov <Mr.Kozelberg@yandex.ru>
2022-10-10 00:56:32 +0300

    i dont know how it is possible to solve eigenvalue problem via iteration method with shift

--- task_3/src/src.f90
+++ task_3/src/src.f90
@@ -37,45 +37,90 @@ contains
 		deallocate(z)
 	end subroutine dealloc
 
-	subroutine iterations_method()
+	subroutine iterations_method(iter_num)
 		implicit none
-		real(8) :: d(M-1), e(M-2), H(M-1,M-1), H_(M-1,M-2)
-		real(8) :: E_max, phi_1(M-1), phi_2(M-1)
+		integer, intent(in) :: iter_num
+		real(8) :: H(M-1,M-1), H_(M-1,M-1), id(M-1,M-1)
+		real(8) :: E_max, E_min, phi_1(M-1), phi_2(M-1)
 
-		do i = 1, ! <---
+		! Hamiltonian
+		H(:,:) = 0.0
+		do i = 1, M-1
+			H(i,i) = diag(i)
+			if (i .eq. 1) then
+				H(i,i+1) = subdiag(i)
+			else if (i .eq. M-1) then
+				H(i-1,i) = subdiag(i)
+			else
+				H(i,i+1) = subdiag(i)
+				H(i-1,i) = subdiag(i)
+			end if
+		end do
+
+		! phi_1 = [(exp(-x(i)**2), i=1, M-1)]
+		! phi_1 = phi_1 / norm2(phi_1)
+		phi_1(:) = 1/delta
+		do i = 1, 1000
+			phi_2 = matmul(H, phi_1)
+			phi_2 = phi_2 / norm2(phi_2)
+			phi_1 = phi_2
+		end do
+
+		E_max = dot_product(matmul(H, phi_1), phi_1)
+
+		print *, 'E_max is ', E_max
+
+		id(:,:) = 0.0
+		do i = 1, M-1
+			id(i,i) = 1.0
+		end do
+
+		H_ = H - E_max/2.0 * id
+
+		phi_1 = [(1/cosh(x(i))**(2), i=1, M-1)]
+		phi_1 = phi_1 / norm2(phi_1)
+		do i = 1, iter_num
+			phi_2 = matmul(H_, phi_1)
+			phi_2 = phi_2 / norm2(phi_2)
+			phi_1 = phi_2
+		end do
+
+		E_min = dot_product(matmul(H_, phi_1), phi_1)
+
+		print *, 'E_1 is ', E_min + E_max/2.0
 
 
 	end subroutine iterations_method
 	
-   	subroutine T_vs_M()
-   		integer, dimension(300) :: mm = [(100*i, i=1, 300)]
+   	! subroutine T_vs_M()
+   	! 	integer, dimension(300) :: mm = [(100*i, i=1, 300)]
 
-      	real(8) :: rr = 6.0, v0 = -1.0
-      	real(4) :: start_time, finish_time
-      	integer :: io
+    !   	real(8) :: rr = 6.0, v0 = -1.0
+    !   	real(4) :: start_time, finish_time
+    !   	integer :: io
 
-      	open(newunit=io, file="../output/T_vs_M.txt")
+    !   	open(newunit=io, file="../output/T_vs_M.txt")
 
-      	write(io, *) '# M # T, sec' 
+    !   	write(io, *) '# M # T, sec' 
 
-      	do i = 1, size(mm)
+    !   	do i = 1, size(mm)
 
-         	call cpu_time(start_time)
+    !      	call cpu_time(start_time)
 
-         	call init(v0, rr, mm(i))
+    !      	call init(v0, rr, mm(i))
 
-         	call solver()
+    !      	call solver()
 
-         	call cpu_time(finish_time)
+    !      	call cpu_time(finish_time)
 
-         	call dealloc()
+    !      	call dealloc()
 
-         	write(io, *) M, finish_time - start_time
+    !      	write(io, *) M, finish_time - start_time
 
-      	end do
+    !   	end do
 
-      	close(io)
-   	end subroutine T_vs_M
+    !   	close(io)
+   	! end subroutine T_vs_M
 
 end module task
 
@@ -84,14 +129,14 @@ program src
 	implicit none
 
 	! test
-	! real(8) :: v0, rr
-	! integer :: mm
+	real(8) :: v0, rr
+	integer :: mm
 
-	! v0 = -5.0; rr = 15.0; mm = 30000
+	v0 = -1.0; rr = 6.0; mm = 1000
 
-	! call init(v0, rr, mm)
+	call init(v0, rr, mm)
 
-	! call solver()
+	call iterations_method(10000)
 
 	! write(*, *) 'N = ', N
 
@@ -106,5 +151,5 @@ program src
 	! call T_vs_M()
 
 	! Third part of the task
-	call E_vs_M_vs_R(real(-1.0,8),"-1")
+	! call E_vs_M_vs_R(real(-1.0,8),"-1")
 end program src
